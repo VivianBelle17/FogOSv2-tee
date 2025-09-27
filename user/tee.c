18a7664c8d0b6161401881fb7f42cf8d9fc54f13
Vivian Pleitez <vbpleitez@gojira.cs.usfca.edu>
2025-09-26 23:05:20 -0700

    Able to add writing to multiple files

--- user/tee.c
+++ user/tee.c
@@ -10,14 +10,13 @@
 
 int main(int argc, char *argv[]){
 
+	for(int i = 0; i < argc; i++){
+		printf("%d: %s\n", i, argv[i]);
+	}
+
+
 	char data[16];
 	int bytesToRead;
-
-	// Opening file. Create if it doesn't exists and write to it
-	int output = open("test.txt", O_CREATE | O_WRONLY);
-	if(output < 0){
-		printf("Error in opening file\n");
-	}
 
 
 	while((bytesToRead = read(0, data, sizeof(data))) > 0){
@@ -26,11 +25,42 @@ int main(int argc, char *argv[]){
 		write(1, data, bytesToRead);
 
 		// Writing to a file
-		write(output, data, bytesToRead);
+		//write(output, data, bytesToRead);
+		
+		for(int i = 1; i < argc; i++){
 
+			// Opening file. Create if it doesn't exists and write to it
+			int output = open(argv[i], O_CREATE | O_WRONLY);
+			
+			if(output < 0){
+				printf("Error in opening file\n");
+			}
+
+			// Writing to a file
+			write(output, data, bytesToRead);
+			close(output);
+		}
 	}
+	//bytesToRead is still 0 so it doesn't go in the while loop the second time around
+	// while loop first, then outside of it the for loop thats writing to the files
+        
+	// Handles multiple files
+	
+	
+	//for(int i = 1; i < argc; i++){
 
-	close(output);
+		// Opening file. Create if it doesn't exists and write to it
+	//	int output = open(argv[i], O_CREATE | O_WRONLY);
+	//	if(output < 0){
+	//		printf("Error in opening file\n");
+	//	}
+
+		// Writing to a file
+	//	write(output, data, bytesToRead);
+	//	close(output);
+	//}
+
+	//close(output);
 
 	return 0;
 
