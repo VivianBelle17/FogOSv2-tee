a7614ee92d6d104794eea32ebf68c5201ad8e394
Vivian Pleitez <vbpleitez@gojira.cs.usfca.edu>
2025-09-26 22:25:03 -0700

    Added write to a file

--- user/tee.c
+++ user/tee.c
@@ -1,24 +1,36 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include "kernel/fcntl.h"
 
+/*
+ * fd 0 = standard input (keyboard, pipe, etc)
+ * fd 1 = standard output (terminal)
+ */
 
 int main(int argc, char *argv[]){
-
-
-	/*for(int i = 1; i < argc; i++){	
-
-		// Writing to the terminal
-		write(1, *(argv + i), sizeof(*(argv + 1)));
-	}*/
 
 	char data[16];
 	int bytesToRead;
 
-	while((bytesToRead = read(0, data, sizeof(data))) > 0){
-		write(1, data, bytesToRead);
+	// Opening file. Create if it doesn't exists and write to it
+	int output = open("test.txt", O_CREATE | O_WRONLY);
+	if(output < 0){
+		printf("Error in opening file\n");
 	}
 
+
+	while((bytesToRead = read(0, data, sizeof(data))) > 0){
+		
+		// Writing to the terminal
+		write(1, data, bytesToRead);
+
+		// Writing to a file
+		write(output, data, bytesToRead);
+
+	}
+
+	close(output);
 
 	return 0;
 
