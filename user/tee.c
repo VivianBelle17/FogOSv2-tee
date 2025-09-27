2a9e88dbca8831edcdfead3de1ad804677a8127a
Vivian Pleitez <vbpleitez@gojira.cs.usfca.edu>
2025-09-26 21:43:25 -0700

    Added reading from and writing to the terminal

--- user/tee.c
+++ user/tee.c
@@ -6,10 +6,17 @@
 int main(int argc, char *argv[]){
 
 
-	for(int i = 1; i < argc; i++){
+	/*for(int i = 1; i < argc; i++){	
 
 		// Writing to the terminal
 		write(1, *(argv + i), sizeof(*(argv + 1)));
+	}*/
+
+	char data[16];
+	int bytesToRead;
+
+	while((bytesToRead = read(0, data, sizeof(data))) > 0){
+		write(1, data, bytesToRead);
 	}
 
 
